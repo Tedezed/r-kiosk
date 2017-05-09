2f8577141bbfa04f724ed67529b2004eb4b926a9
Juanma <juanmanuel.torres@guadaltech.es>
2017-05-09 09:07:39 +0200

    open

--- src/content/rkioskbrowser.js
+++ src/content/rkioskbrowser.js
@@ -14,9 +14,12 @@ function Rkiosk_navbar_setting()
   var rkiosk_navbar_enable="true";
   var prefs = Components.classes["@mozilla.org/preferences-service;1"].
   	getService(Components.interfaces.nsIPrefBranch);
-  if (prefs.getPrefType("rkiosk.navbar") == prefs.PREF_BOOL){
+   if (prefs.getPrefType("rkiosk.navbar") == prefs.PREF_BOOL){
     if (prefs.getBoolPref("rkiosk.navbar")) rkiosk_navbar_enable = "false";
   }
+  //
+  var rkiosk_navbar_enable="false";
+  //
   var rkiosk_element = document.getElementById("navigator-toolbox");
   rkiosk_element.setAttribute("hidden", rkiosk_navbar_enable);
 }
